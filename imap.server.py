cf9f7915fe92e93788cf6949592ad41545f672ca
Michał Żmudziak <Micha mudziak>
2017-04-11 10:58:27 +0200

    Initial commit (working implementation)

---
+++ imap.server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import socket as s
+
+S = "TO"
+
+
+def recv_all_until(sockfd, crlf):
+    data = ""
+    while not data.endswith(crlf):
+        data = data + sockfd.recv(1)
+    print data
+
+
+ADDRESS = "212.182.24.27"
+PORT = 143
+EMAIL = "pasumcs@infumcs.edu"
+PASSWORD = "P4SInf2017"
+CRLF = "\r\n"
+
+SERVER_ADDRESS = (ADDRESS, PORT)
+sock = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+sock.connect(SERVER_ADDRESS)
+recv_all_until(sock, CRLF)
+
+MSG_LOGIN_TAG = "T0"
+MSG_LOGIN = MSG_LOGIN_TAG + " LOGIN " + EMAIL + " " + PASSWORD + CRLF
+sock.sendall(MSG_LOGIN)
+recv_all_until(sock, MSG_LOGIN_TAG)
+
+MSG_SELECT_TAG = "T1"
+MSG_SELECT = MSG_SELECT_TAG + " SELECT Inbox" + CRLF
+sock.sendall(MSG_SELECT)
+recv_all_until(sock, MSG_SELECT_TAG)
+
+MSG_FETCH_TAG = "T2"
+MSG_FETCH = MSG_FETCH_TAG + " FETCH 1 BODY[]" + CRLF
+sock.sendall(MSG_FETCH)
+recv_all_until(sock, MSG_FETCH_TAG)
+
+MSG_CLOSE_TAG = "T3"
+MSG_CLOSE = MSG_CLOSE_TAG + " CLOSE" + CRLF
+sock.sendall(MSG_CLOSE)
+recv_all_until(sock, MSG_CLOSE_TAG)
+
+MSG_LOGOUT_TAG = "T4"
+MSG_LOGOUT = MSG_LOGOUT_TAG + " LOGOUT" + CRLF
+sock.sendall(MSG_LOGOUT)
+recv_all_until(sock, MSG_LOGOUT_TAG)
